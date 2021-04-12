a21bf76e21d0eace04ba4e07dc78bc1f4aa32c3d
Leonardo Augusto <leo.augustobh13@gmail.com>
2021-04-12 14:19:11 -0300

    Projeto Spring API

---
+++ src/test/java/com/pluralsight/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencedemo;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
